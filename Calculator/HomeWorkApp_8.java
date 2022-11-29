3b22d554dc9a2e91c3e873cce1e9c33183284cf5
AHTOH <IlinAN89@yandex.ru>
2022-11-29 18:55:12 +1000

    Homework_8

---
+++ Calculator/HomeWorkApp_8.java
@@ -0,0 +1,7 @@
+package Java_homework.Java_homework.Calculator;
+
+public class HomeWorkApp_8 {
+    public static void main(String[] args) {
+        new ApplicationForm("Calculator v1.1");
+    }
+}
---
+++ Calculator/components/DigitJButton.java
@@ -0,0 +1,12 @@
+package Java_homework.Java_homework.Calculator.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DigitJButton extends JButton {
+    public DigitJButton(String text) {
+        super(text);
+        setFont(new Font("TimesRoman", Font.PLAIN, 25));
+        setBackground(new Color(70, 159, 231));
+    }
+}
